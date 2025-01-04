f3503c99c2decf131685d20bfa44c3dc76c6de99
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2025-01-05 00:42:54 +0530

    update index.js

--- 05-Backend-Project/src/index.js
+++ 05-Backend-Project/src/index.js
@@ -8,3 +8,30 @@ dotenv.config({
 })
 
 connectDB()
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
